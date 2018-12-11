0ab22cef8f0558fa1d3618024a184fc05430fc39
Samuel Harper <srharper@CedarvilleUniversity394.onmicrosoft.com>
2018-12-11 16:15:16 -0500

    nuget

--- ReleaseCand/Controllers/OnOrdsController.cs
+++ ReleaseCand/Controllers/OnOrdsController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ReleaseCand.Models;
+using ReleaseCand.ViewModels;
+
 
 namespace ReleaseCand.Controllers
 {
---
+++ ReleaseCand/Migrations/20181211211438_InitialMigration.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ReleaseCand.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OnOrd",
+                columns: table => new
+                {
+                    OnOrdID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    LastName = table.Column<string>(nullable: true),
+                    FirstName = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    CheeseBurger = table.Column<bool>(nullable: false),
+                    Lasagna = table.Column<bool>(nullable: false),
+                    TiriMisu = table.Column<bool>(nullable: false),
+                    CheeseCake = table.Column<bool>(nullable: false),
+                    FrenchFries = table.Column<bool>(nullable: false),
+                    Pepsi = table.Column<bool>(nullable: false),
+                    Sprite = table.Column<bool>(nullable: false),
+                    Water = table.Column<bool>(nullable: false),
+                    BillAddress = table.Column<string>(nullable: true),
+                    BillCity = table.Column<string>(nullable: true),
+                    BillState = table.Column<string>(nullable: true),
+                    BillZip = table.Column<string>(nullable: true),
+                    CreditCardNum = table.Column<string>(nullable: true),
+                    CCV = table.Column<string>(nullable: true),
+                    ExpDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OnOrd", x => x.OnOrdID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OnOrd");
+        }
+    }
+}
